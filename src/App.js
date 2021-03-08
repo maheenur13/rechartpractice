f56bc4d7985dc15603f11ec4a5607a5cf4f62cf3
maheenur13 <differentmahee13@gmail.com>
2021-03-08 18:18:01 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Chart from './Components/Chart/Chart';
+import CgpaChart from './Components/CgpaChart/CgpaChart';
 
 function App() {
   return (
     <div>
       <Chart></Chart>
+      <CgpaChart></CgpaChart>
     </div>
   );
 }
